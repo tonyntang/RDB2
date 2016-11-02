afce47bd7b727e2152c912ce0290a6d3e3e62180
Ning Tang <tangning@gmail.com>
2016-11-02 13:11:55 -0400

    Update login.R
    add check box for saving credential

--- R/login.R
+++ R/login.R
@@ -5,8 +5,11 @@ getLoginDetails <- function(user, passwd)
   ui <- miniPage(
     gadgetTitleBar("IBM DB2 Credential Imput"),
     miniContentPanel(
-      textInput("user", "Login ID:", value = user),
-      passwordInput("pass", "Password:", value = passwd)
+      textInput("user_txt", "Login ID:", value = user),
+      checkboxInput("user_ckbx", label = "Remember me?", value = TRUE),
+
+      passwordInput("pass_txt", "Password:", value = passwd),
+      checkboxInput("pass_ckbx", label = "Save the password?", value = TRUE)
     )
   )
 
@@ -16,8 +19,10 @@ getLoginDetails <- function(user, passwd)
     # When the Done button is clicked, return a value
     observeEvent(input$done,
                  {
-                   returnValue <- list(loginID = input$user,
-                                       password = input$pass,
+                   returnValue <- list(loginID = input$user_txt,
+                                       password = input$pass_txt,
+                                       save_user = input$user_ckbx,
+                                       save_pass = input$pass_ckbx,
                                        success = 1)
                    stopApp(returnValue)
                    })
@@ -25,10 +30,12 @@ getLoginDetails <- function(user, passwd)
     observeEvent(input$cancel,
                  {
 
-                   returnValue <- list(loginID = input$user,
-                                       password = input$pass,
+                   returnValue <- list(loginID = input$user_txt,
+                                       password = input$pass_txt,
+                                       save_user = input$user_ckbx,
+                                       save_pass = input$pass_ckbx,
                                        success = -1)
-                   stopApp(NULL)
+                   stopApp(returnValue)
                  })
   }
 
