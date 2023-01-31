c3a48aa82bd0959342309232380c2d0b721e4aad
Ning Tang <tangning@gmail.com>
2023-01-31 11:24:39 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1,18 +1,28 @@
-infoBox_ui <- function(id) {
+trans_plot_ui <- function(id) {
   ns <- NS(id)
   
-  infoBoxOutput(ns('infobox'))
+  plotlyOutput(ns('trans_plot'))
 }
 
-infoBox_server <- function(id, dp, ...) {
+trans_plot_server <- function(id, dp, direction = 'both') {
   moduleServer(
     id,
     function(input, output, session) {
-      output$infobox <- renderInfoBox({
+      
+      output$trans_plot <- renderPlotly({
         if(is.null(dp)) return(NULL)
         
-        infoBox(value = dp, ...)
+        # begin plotly
+        dp %>%
+          plot_ly(
+            x = ~month,
+            y = ~total_amt_usd,
+            mode = 'markers',
+            type = 'scatter',
+            color = ~dia_trans_direction,
+            linetype = ~dia_trans_direction
+            )
       })
-    }
+    }  
   )
 }
