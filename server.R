6c6d46e77516f68f5d65df101108ced728719d46
Ning Tang <tangning@gmail.com>
2023-01-31 11:26:08 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1,28 +1,56 @@
-trans_plot_ui <- function(id) {
-  ns <- NS(id)
-  
-  plotlyOutput(ns('trans_plot'))
-}
+library(progress)
+library(crayon)
 
-trans_plot_server <- function(id, dp, direction = 'both') {
-  moduleServer(
-    id,
-    function(input, output, session) {
-      
-      output$trans_plot <- renderPlotly({
-        if(is.null(dp)) return(NULL)
-        
-        # begin plotly
-        dp %>%
-          plot_ly(
-            x = ~month,
-            y = ~total_amt_usd,
-            mode = 'markers',
-            type = 'scatter',
-            color = ~dia_trans_direction,
-            linetype = ~dia_trans_direction
-            )
-      })
-    }  
+read_all_jp_data <- function(path, exclude = NULL, skip = 0) {
+  
+  files <- dir(path, pattern = '.*xlsx$', full.names = TRUE, all.files = FALSE)
+  
+  if(!is.null(exclude)) {
+    files <- subset(files, !(basename(files) %in% exclude))
+    
+    cat('The following files are excluded ========== \n')
+    cat(green(str_c(exclude, '\n')))
+    cat('\n')
+  }
+  
+  # create progress bar --------------------------------------------------------
+  bar_total <- length(files)
+  pb <- progress_bar$new(
+    format = '(:percent) [:bar] Reading :what (:current/:total)  time elapsed: [:elapsed] eta: [:eta]',
+    total = bar_total,
+    clear = FALSE
   )
+  pb$tick(0)
+  
+  map_dfr(
+    files,
+    function(x) {
+      
+      pb$tick(tokens = list(what = basename(x)))
+      # read all in as text ------------------------------------------------------
+      read_excel(x, col_types = 'text', skip = skip) %>% 
+        filter(!is.na(transfer_key)) %>% 
+        mutate(file_nm = basename(x)) %>% 
+        mutate(
+          across(contains('_date'), 
+                 function(x) {
+                   as.numeric(x) %>%                    
+                     as_date(origin = '1899-12-30')
+                })
+        ) %>% 
+        # convert to numeric -----------------------------------------------------
+        mutate(
+          across(contains('_amount'),
+                 function(x) {
+                   str_replace_all(x, ',', '') %>% 
+                     as.numeric()
+                })
+        ) %>% 
+        mutate(
+          across(where(is.character), ~ replace_na(.x, ''))
+        )
+    }) %>% 
+    group_by(transfer_key) %>% 
+    filter(row_number() == 1L) %>% 
+    ungroup
 }
